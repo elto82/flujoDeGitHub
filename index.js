4229f1272bbae9d8c4dc0af94876bed9010e8ade
kaylreese <sbocanegrad@unsm.edu.pe>
2023-04-24 18:06:51 -0500

    ejercicio

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 function laCajaDePandora(numero) {
   // proximamente escribiremos codigo aqui
+  if(numero % 2 == 0){
+    let x=1;
+    let binario = 0;
+    while(numero >= 1) {
+      if(numero % 2 == 1){
+        binario = binario + x;
+      }
+
+      numero = n / 2;
+      x = x * 10;
+    }
+    return binario;
+  } else {
+    return (numero).toString(16)
+  }
 }

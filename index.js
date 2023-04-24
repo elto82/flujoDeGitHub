c1614df8ca3d0f3c5947058b1b70b4ac94c962cb
Argiro <elto.82@gmai.com>
2023-04-24 17:52:15 -0500

    funcion binario hexadecimal

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 function laCajaDePandora(numero) {
   // proximamente escribiremos codigo aqui
+  if (typeof numero !== "number") {
+    throw new Error("El argumento debe ser un número");
+  }
+
+  if (numero % 2 === 0) {
+    return numero.toString(2);
+  } else {
+    return numero.toString(16);
+  }
 }
